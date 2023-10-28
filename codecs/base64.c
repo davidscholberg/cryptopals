34514c43e88d8aaa59265f24a6ce084debd9ff48
David Scholberg <recombinant.vector@gmail.com>
2023-10-28 00:38:03 -0400

    renamed base64 digits array

--- codecs/base64.c
+++ codecs/base64.c
@@ -4,7 +4,7 @@
 
 #define base64_padding_character '='
 
-static const char *const base64_alphabet =
+static const char *const base64_digits =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 bool bytes_to_base64(
@@ -20,36 +20,36 @@ bool bytes_to_base64(
     int base64_index = 0;
     for (int byte_index = 0; byte_index < byte_buffer_size; byte_index += 3) {
         int sextet_value = byte_buffer[byte_index] >> 2;
-        base64_buffer[base64_index] = base64_alphabet[sextet_value];
+        base64_buffer[base64_index] = base64_digits[sextet_value];
         base64_index++;
 
         sextet_value = (byte_buffer[byte_index] & 0x3) << 4;
         if (byte_index + 1 >= byte_buffer_size) {
-            base64_buffer[base64_index] = base64_alphabet[sextet_value];
+            base64_buffer[base64_index] = base64_digits[sextet_value];
             base64_buffer[base64_index + 1] = base64_padding_character;
             base64_buffer[base64_index + 2] = base64_padding_character;
             base64_index += 3;
             break;
         } else {
             sextet_value += byte_buffer[byte_index + 1] >> 4;
-            base64_buffer[base64_index] = base64_alphabet[sextet_value];
+            base64_buffer[base64_index] = base64_digits[sextet_value];
             base64_index++;
         }
 
         sextet_value = (byte_buffer[byte_index + 1] & 0xf) << 2;
         if (byte_index + 2 >= byte_buffer_size) {
-            base64_buffer[base64_index] = base64_alphabet[sextet_value];
+            base64_buffer[base64_index] = base64_digits[sextet_value];
             base64_buffer[base64_index + 1] = base64_padding_character;
             base64_index += 2;
             break;
         } else {
             sextet_value += byte_buffer[byte_index + 2] >> 6;
-            base64_buffer[base64_index] = base64_alphabet[sextet_value];
+            base64_buffer[base64_index] = base64_digits[sextet_value];
             base64_index++;
         }
 
         sextet_value = byte_buffer[byte_index + 2] & 0x3f;
-        base64_buffer[base64_index] = base64_alphabet[sextet_value];
+        base64_buffer[base64_index] = base64_digits[sextet_value];
         base64_index++;
     }
 
