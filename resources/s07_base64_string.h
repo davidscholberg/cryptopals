197ccb832b855fcaa33c0b34c1b45df295754828
David Scholberg <recombinant.vector@gmail.com>
2023-11-18 20:14:18 -0500

    refactored solution 7 to use resource text file

--- resources/s07_base64_string.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-extern const char* const s07_base64_string;
--- solutions/s07.c
+++ solutions/s07.c
@@ -1,27 +1,37 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "ciphers/aes.h"
 #include "codecs/base64.h"
-#include "resources/s07_base64_string.h"
+#include "utility/files.h"
 
 #define block_size 16
 
 // Challenge 7: Decrypt the base64-encoded string using AES in ECB mode with key "YELLOW SUBMARINE".
 bool s07(char* const out_buffer, const int out_buffer_size) {
-    const char* const key = "YELLOW SUBMARINE";
-    int input_bytes_size = 0;
-    if (!base64_to_bytes_size(s07_base64_string, &input_bytes_size)) {
-        return false;
-    }
-    if (input_bytes_size % block_size != 0 || input_bytes_size > out_buffer_size) {
-        return false;
-    }
-    if (!base64_to_bytes(s07_base64_string, (unsigned char* const)out_buffer, input_bytes_size)) {
+    char* base64_string = file_to_string("s07_base64_string.txt");
+    if (!base64_string) {
         return false;
     }
 
+    int input_bytes_size = 0;
+    if (!base64_to_bytes_size(base64_string, &input_bytes_size)) {
+        free(base64_string);
+        return false;
+    }
+    if (input_bytes_size % block_size != 0 || input_bytes_size > out_buffer_size) {
+        free(base64_string);
+        return false;
+    }
+    if (!base64_to_bytes(base64_string, (unsigned char* const)out_buffer, input_bytes_size)) {
+        free(base64_string);
+        return false;
+    }
+    free(base64_string);
+
+    const char* const key = "YELLOW SUBMARINE";
     if (!aes_ecb_decrypt(
             (unsigned char* const)out_buffer,
             input_bytes_size,
