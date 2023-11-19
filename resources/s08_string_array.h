fbd1a4a6d762e0522466b385d51082422d496135
David Scholberg <recombinant.vector@gmail.com>
2023-11-18 20:33:20 -0500

    refactored solution 8 to use resource text file

--- resources/s08_string_array.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-extern const char* const s08_string_array[];
-extern const int s08_string_array_size;
--- solutions/s08.c
+++ solutions/s08.c
@@ -1,29 +1,43 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "analysis/repeating_blocks.h"
+#include "codecs/decode_lines.h"
 #include "codecs/hex.h"
-#include "resources/s08_string_array.h"
+#include "utility/files.h"
 
 #define block_size 16
 
 // Challenge 8: Detect which input string has been encrypted with AES ECB.
 bool s08(char* const out_buffer, __attribute__((unused)) const int out_buffer_size) {
-    int guessed_index = 0;
-    for (int i = 0; i < s08_string_array_size; i++) {
-        const int input_bytes_size = hex_to_bytes_size(strlen(s08_string_array[i]));
-        unsigned char input_bytes[input_bytes_size];
-        if (!hex_to_bytes(s08_string_array[i], input_bytes, input_bytes_size)) {
-            return false;
-        }
+    int line_count = 0;
+    char** lines = file_to_lines("s08_hex_string_array.txt", &line_count);
+    if (!lines) {
+        return false;
+    }
 
-        if (repeating_blocks(input_bytes, input_bytes_size, block_size)) {
+    int* buffer_sizes = 0;
+    unsigned char** buffers =
+        decode_lines(lines, line_count, hex_to_bytes, hex_to_bytes_size_wrapper, &buffer_sizes);
+    if (!buffers) {
+        free(lines);
+        return false;
+    }
+
+    int guessed_index = 0;
+    for (int i = 0; i < line_count; i++) {
+        if (repeating_blocks(buffers[i], buffer_sizes[i], block_size)) {
             guessed_index = i;
             break;
         }
     }
 
-    strcpy(out_buffer, s08_string_array[guessed_index]);
+    strcpy(out_buffer, lines[guessed_index]);
+
+    free(lines);
+    free(buffers);
+
     return true;
 }
