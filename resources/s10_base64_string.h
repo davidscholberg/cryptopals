5d55c1a0ea6690ec6775573f7d908cd061f06487
David Scholberg <recombinant.vector@gmail.com>
2023-11-18 20:52:10 -0500

    refactored solution 10 to use resource text file

--- resources/s10_base64_string.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-extern const char* const s10_base64_string;
--- solutions/s10.c
+++ solutions/s10.c
@@ -1,23 +1,33 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "ciphers/aes.h"
 #include "codecs/base64.h"
-#include "resources/s10_base64_string.h"
+#include "utility/files.h"
 
 // Challenge 10: Decrypt the input using AES in CBC mode with the given key and iv.
 bool s10(char* const out_buffer, const int out_buffer_size) {
+    char* base64_string = file_to_string("s10_base64_string.txt");
+    if (!base64_string) {
+        return false;
+    }
+
     int input_bytes_size = 0;
-    if (!base64_to_bytes_size(s10_base64_string, &input_bytes_size)) {
+    if (!base64_to_bytes_size(base64_string, &input_bytes_size)) {
+        free(base64_string);
         return false;
     }
     if (input_bytes_size % aes_block_size != 0 || input_bytes_size > out_buffer_size) {
+        free(base64_string);
         return false;
     }
-    if (!base64_to_bytes(s10_base64_string, (unsigned char* const)out_buffer, input_bytes_size)) {
+    if (!base64_to_bytes(base64_string, (unsigned char* const)out_buffer, input_bytes_size)) {
+        free(base64_string);
         return false;
     }
+    free(base64_string);
 
     const char* const key = "YELLOW SUBMARINE";
     const unsigned char iv[aes_block_size] = {0};
