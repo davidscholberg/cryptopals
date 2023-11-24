122b5e90d260c2cfd4747623766c28a1ec09d874
David Scholberg <recombinant.vector@gmail.com>
2023-11-24 15:20:38 -0500

    moved solution function declaration to macro

--- solutions/solutions.h
+++ solutions/solutions.h
@@ -1,24 +1,27 @@
 #pragma once
 
-bool s01(char* const out_buffer, const int out_buffer_size);
-bool s02(char* const out_buffer, const int out_buffer_size);
-bool s03(char* const out_buffer, const int out_buffer_size);
-bool s04(char* const out_buffer, const int out_buffer_size);
-bool s05(char* const out_buffer, const int out_buffer_size);
-bool s06(char* const out_buffer, const int out_buffer_size);
-bool s07(char* const out_buffer, const int out_buffer_size);
-bool s08(char* const out_buffer, const int out_buffer_size);
-bool s09(char* const out_buffer, const int out_buffer_size);
-bool s10(char* const out_buffer, const int out_buffer_size);
-bool s11(char* const out_buffer, const int out_buffer_size);
-bool s12(char* const out_buffer, const int out_buffer_size);
-bool s13(char* const out_buffer, const int out_buffer_size);
-bool s14(char* const out_buffer, const int out_buffer_size);
-bool s15(char* const out_buffer, const int out_buffer_size);
-bool s16(char* const out_buffer, const int out_buffer_size);
-bool s17(char* const out_buffer, const int out_buffer_size);
-bool s18(char* const out_buffer, const int out_buffer_size);
-bool s19(char* const out_buffer, const int out_buffer_size);
-bool s20(char* const out_buffer, const int out_buffer_size);
-bool s21(char* const out_buffer, const int out_buffer_size);
-bool s22(char* const out_buffer, const int out_buffer_size);
+// Expands to the solution function declaration for the given solution number.
+#define solution_declaration(n) bool s##n(char* const out_buffer, const int out_buffer_size)
+
+solution_declaration(01);
+solution_declaration(02);
+solution_declaration(03);
+solution_declaration(04);
+solution_declaration(05);
+solution_declaration(06);
+solution_declaration(07);
+solution_declaration(08);
+solution_declaration(09);
+solution_declaration(10);
+solution_declaration(11);
+solution_declaration(12);
+solution_declaration(13);
+solution_declaration(14);
+solution_declaration(15);
+solution_declaration(16);
+solution_declaration(17);
+solution_declaration(18);
+solution_declaration(19);
+solution_declaration(20);
+solution_declaration(21);
+solution_declaration(22);
